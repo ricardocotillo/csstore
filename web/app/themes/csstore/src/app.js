b9ea6754960b4aa7248ac4a3b64fbdf57078557b
ricardocotillo <ricardo.cotillo@gmail.com>
2023-01-15 20:55:41 -0500

    added search

--- web/app/themes/csstore/src/app.js
+++ web/app/themes/csstore/src/app.js
@@ -2,4 +2,53 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+// search
+window.searchData = () => {
+  return {
+    controller: new AbortController(),
+    searching: false,
+    result: [],
+    open: false,
+    t: '',
+    init() {
+      window.addEventListener('click', e => {
+        if (this.open && !e.target.classList.contains('search')) {
+          this.open = false
+        }
+      })
+    },
+    empty() {
+      this.open = false
+      this.result = []
+      this.t = ''
+    },
+    async search(e) {
+      const t = e.target.value
+      if (this.searching) {
+        this.controller.abort()
+        if (t.length === 0) {
+          this.empty()
+          return
+        } else {
+          this.controller = new AbortController()
+        }
+      }
+      this.searching = true
+      fetch(`/wp-json/wp/v2/product/?search=${t}`, {signal: this.controller.signal})
+        .then(res => {
+          this.searching = false
+          return res.json()
+        })
+        .then(j => {
+          this.result = j
+          console.log(j)
+          this.open = true
+        })
+    },
+    async makeRequest() {
+
+    }
+  }
+}
+
 Alpine.start()
