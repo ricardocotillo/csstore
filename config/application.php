f9ba177e729a6674876522655ca205de63eed586
ricardocotillo <ricardo.cotillo@gmail.com>
2023-02-06 23:00:29 -0500

    cache

--- config/application.php
+++ config/application.php
@@ -113,6 +113,7 @@ Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', false);
 Config::define('SCRIPT_DEBUG', false);
 Config::define('IS_VITE_DEVELOPMENT', false);
+Config::define( 'WP_CACHE', true );
 ini_set('display_errors', '0');
 
 /**
